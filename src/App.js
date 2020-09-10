bedeeb5ad39f442ff972188f0a9d23c0a397e379
Dromediansk <miroslav.pillar@senacor.com>
2020-09-10 18:13:06 +0200

    setting up

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
 import "./App.css";
-
 import HomePage from "./homepage.component";
 
 function App() {
-    return <HomePage />;
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
 }
 
 export default App;
