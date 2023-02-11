02639f96d08d9829264b4300eb69da0fb5068712
Dromediansk <miroslav.pillar@globallogic.com>
2023-02-11 10:01:34 +0100

    memoizing selectors with reselect library to prevent from re-rendering

--- src/store/categories/category.selector.js
+++ src/store/categories/category.selector.js
@@ -1,6 +1,18 @@
-export const selectCategoriesByTitle = (state) =>
-  state.categories.categories.reduce((categories, category) => {
-    const { title, items } = category;
-    categories[title.toLowerCase()] = items;
-    return categories;
-  }, {});
+import { createSelector } from "reselect";
+
+const selectCategoryReducer = (state) => state.categories;
+
+export const selectCategories = createSelector(
+  [selectCategoryReducer],
+  (categoriesSlice) => categoriesSlice.categories
+);
+
+export const selectCategoriesByTitle = createSelector(
+  [selectCategories],
+  (categories) =>
+    categories.reduce((acc, category) => {
+      const { title, items } = category;
+      acc[title.toLowerCase()] = items;
+      return acc;
+    }, {})
+);
