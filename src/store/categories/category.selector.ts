4c37e21eec5532f186d4f335fec36304e175548f
Dromediansk <miroslav.pillar@globallogic.com>
2023-02-15 08:52:18 +0100

    typing store

--- src/store/categories/category.selector.ts
+++ src/store/categories/category.selector.ts
@@ -1,8 +1,10 @@
 import { createSelector } from "reselect";
+import { RootState } from "../store";
 import { CategoriesState } from "./category.reducer";
 import { CategoryByTitle } from "./category.types";
 
-const selectCategoryReducer = (state): CategoriesState => state.categories;
+const selectCategoryReducer = (state: RootState): CategoriesState =>
+  state.categories;
 
 export const selectCategories = createSelector(
   [selectCategoryReducer],
